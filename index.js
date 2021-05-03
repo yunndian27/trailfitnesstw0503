40cbabcc622681303b75702972e1c4f61d931f0a
yunndian27 <yunjen1225@gmail.com>
2021-05-03 10:21:25 +0800

    機器人基本東西

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 import linebot from 'linebot'
+import dotenv from 'dotenv'
+
+// 讓套件讀取 .env 檔案
+// 讀取後可以用 process.env.變數 使用
+dotenv.config()
+
+const bot = linebot({
+  channelId: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+})
+
+bot.listen('/', process.env.PORT)
+
+bot.on('message', event => {
+  console.log(event)
+})
