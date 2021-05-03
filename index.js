be4fa0aac35789f13189ae0271f40522528c5c40
yunndian27 <yunjen1225@gmail.com>
2021-05-03 14:35:25 +0800

    刪除資料

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
 import linebot from 'linebot'
 import dotenv from 'dotenv'
 import axios from 'axios'
-// import cheerio from 'cheerio'
-// import schedule from 'node-schedule'
 
-// const data = []
-
-// const getData = async () => {
-//   axios
-//     .get('https://gis.taiwan.net.tw/XMLReleaseALL_public/Walk_f.json')
-//     .then(response => {
-//       data = response.data
-//     })
-//     .catch()
-// }
-
-// 每天 0 點更新資料
-// schedule.scheduleJob('* * 0 * *', getData)
-// getData()
-
-// 讓套件讀取 .env 檔案
-// 讀取後可以用 process.env.變數 使用
 dotenv.config()
 
 const bot = linebot({
