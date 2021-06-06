088d7e38e87eeb0eea1c18e2955e5fb291157757
yunndian27 <yunjen1225@gmail.com>
2021-06-07 01:00:48 +0800

    內容顯示調整-5

--- index.js
+++ index.js
@@ -42,16 +42,15 @@ bot.on('message', async event => {
 
       let reply = ''
       for (const r of result) {
-        reply += `
-        ${r.Name}
-        \n地址: \n${r.Add}
+        reply += ` ▶${r.Name}
+        \n地址: \n  ${r.Add}
         \n步行時間: ${r.Walkingtime} 小時\n\n
-        ┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄`
+        ┄┄┄┄┄┄┄┄┄┄(✿◠‿◠)`
       }
 
       event.reply(reply)
     } catch (error) {
-      event.reply('發生錯誤')
+      event.reply('發生錯誤～等待修復')
     }
   }
   // 鄉鎮搜尋
@@ -63,15 +62,15 @@ bot.on('message', async event => {
 
       let reply = ''
       for (const r of result) {
-        reply += `▶${r.Name} 
+        reply += ` ▶${r.Name} 
         \n  地址: \n  ${r.Add} 
         \n  步行時間: ${r.Walkingtime} 小時\n\n
-        ┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄`
+        ┄┄┄┄┄┄┄┄┄('・ω・')`
       }
 
       event.reply(reply)
     } catch (error) {
-      event.reply('發生錯誤')
+      event.reply('發生錯誤～等待修復')
     }
   }
   // 步道名搜尋
@@ -97,12 +96,13 @@ bot.on('message', async event => {
         \n  步行時間: ${r.Walkingtime} 小時
         \n\n  特色: ${r.Description}
         \n\n  推薦附近景點: ❊【${r.Landscape}】❊
-        \n\n  ※ 注意事項: ${r.Remarks}`
+        \n\n  ※ 注意事項: ${r.Remarks}
+        \n  •̀.̫•́✧ 疫情退散後～一起重回大自然懷抱吧！`
       }
 
       event.reply(reply)
     } catch (error) {
-      event.reply('發生錯誤')
+      event.reply('發生錯誤～等待修復')
     }
   }
 }
