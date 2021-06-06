00f2c34752a9159600d13f6103cf89c8e1548c49
yunndian27 <yunjen1225@gmail.com>
2021-06-06 17:25:42 +0800

    flex message-test3

--- index.js
+++ index.js
@@ -35,14 +35,23 @@ bot.listen('/', process.env.PORT, () => {
 })
 
 const result = data.filter(result => {
-  return result.Region === event.message.text
+  // const road = result.Name
+  // const regex1 = /.+(?:步道)/gi
+
+  // for (const r of road) {
+  //   if (r.match(regex1)) {
+  //     console.log(r.match(regex1))
+  //     return road === event.message.text
+  //   }
+  // }
+  return result.Name === event.message.text
 })
 
 const flex = {
   type: 'bubble',
   hero: {
     type: 'image',
-    url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
+    url: 'https://images.unsplash.com/uploads/1412533519888a485b488/bb9f9777?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
     size: 'full',
     aspectRatio: '20:13',
     aspectMode: 'cover',
@@ -71,10 +80,10 @@ const flex = {
             type: 'box',
             layout: 'baseline',
             spacing: 'sm',
-            contents: [
+            contnts: [
               {
                 type: 'text',
-                text: '地址',
+                text: '位置:',
                 color: '#aaaaaa',
                 size: 'sm',
                 flex: 1
@@ -96,20 +105,79 @@ const flex = {
             contents: [
               {
                 type: 'text',
-                text: '步行',
+                text: '特色:',
                 color: '#aaaaaa',
                 size: 'sm',
                 flex: 1
               },
               {
                 type: 'text',
-                text: `${result.Walkingtime}`,
+                text: `${result.Description}`,
                 wrap: true,
                 color: '#666666',
                 size: 'sm',
                 flex: 5
               }
             ]
+          },
+          {
+            type: 'box',
+            layout: 'vertical',
+            contents: [
+              {
+                type: 'text',
+                text: '步行時間:',
+                size: 'sm',
+                color: '#aaaaaa'
+              },
+              {
+                type: 'text',
+                text: `${result.Walkingtime} 小時`,
+                flex: 5,
+                size: 'sm',
+                color: '#666666'
+              }
+            ]
+          },
+          {
+            type: 'box',
+            layout: 'vertical',
+            contents: [
+              {
+                type: 'text',
+                text: '推薦附近景點:',
+                size: 'sm',
+                color: '#aaaaaa',
+                margin: 'md'
+              },
+              {
+                type: 'text',
+                text: `${result.Landscape}`,
+                color: '#666666',
+                flex: 5,
+                size: 'sm'
+              }
+            ]
+          },
+          {
+            type: 'box',
+            layout: 'vertical',
+            contents: [
+              {
+                type: 'text',
+                text: '注意事項:',
+                size: 'sm',
+                color: '#aaaaaa',
+                margin: 'md'
+              },
+              {
+                type: '"ext',
+                text: `${result.Remarks}`,
+                size: 'sm',
+                color: '#666666'
+              }
+            ],
+            flex: 5
           }
         ]
       }
@@ -118,50 +186,39 @@ const flex = {
   footer: {
     type: 'box',
     layout: 'vertical',
-    spacing: 'sm',
     contents: [
       {
-        type: 'spacer',
-        size: 'sm'
+        type: 'text',
+        text: '疫情退散後～一起重回大自然懷抱吧！',
+        size: 'xs',
+        align: 'center',
+        color: '#2a9d8f'
       }
     ],
-    flex: 0
+    margin: 'lg',
+    spacing: 'sm',
+    position: 'relative'
   }
 }
 
 bot.on('message', async event => {
-  if (event.message.type === 'text') {
-    const message = {
-      type: 'flex',
-      altText: '這是 flex',
-      contents: {
-        type: 'carousel',
-        contents: [flex]
-      }
-    }
-
-    fs.writeFileSync('aaa.json', JSON.stringify(message, null, 2))
-    event.reply(message)
-  }
-
   // 縣市搜尋
-  // if (event.message.type === 'text') {
-  //   try {
-  //     const result = data.filter(result => {
-  //       return result.Region === event.message.text
-  //     })
+  if (event.message.type === 'text') {
+    try {
+      const result = data.filter(result => {
+        return result.Region === event.message.text
+      })
 
-  //     let reply = ''
-  //     for (const r of result) {
-  //       reply += `${r.Name} \n地址: ${r.Add} \n步行時間: ${r.Walkingtime}小時\n\n`
-  //     }
+      let reply = ''
+      for (const r of result) {
+        reply += `${r.Name} \n地址: ${r.Add} \n步行時間: ${r.Walkingtime}小時\n\n`
+      }
 
-  //     event.reply(reply)
-  //   } catch (error) {
-  //     event.reply('發生錯誤')
-  //   }
-  // }
-
+      event.reply(reply)
+    } catch (error) {
+      event.reply('發生錯誤')
+    }
+  }
   // 鄉鎮搜尋
   if (event.message.type === 'text') {
     try {
@@ -180,30 +237,44 @@ bot.on('message', async event => {
     }
   }
   // 步道名搜尋
+  // if (event.message.type === 'text') {
+  //   try {
+  //     const result = data.filter(result => {
+  //       const road = result.Name
+  //       const regex1 = /.+(?:步道)/gi
+
+  //       for (const r of road) {
+  //         if (r.match(regex1)) {
+  //           console.log(r.match(regex1))
+  //           return road === event.message.text
+  //         }
+  //       }
+  //       return result.Name === event.message.text
+  //     })
+
+  //     let reply = ''
+  //     for (const r of result) {
+  //       reply += `${r.Name} \n位置:${r.Add}\n特色:${r.Description} \n\n步行時間: ${r.Walkingtime}小時\n\n推薦附近景點: ${r.Landscape}\n\n注意事項: ${r.Remarks}`
+  //     }
+
+  //     event.reply(reply)
+  //   } catch (error) {
+  //     event.reply('發生錯誤')
+  //   }
+  // }
+
   if (event.message.type === 'text') {
-    try {
-      const result = data.filter(result => {
-        const road = result.Name
-        const regex1 = /.+(?:步道)/gi
-
-        for (const r of road) {
-          if (r.match(regex1)) {
-            console.log(r.match(regex1))
-            return road === event.message.text
-          }
-        }
-        return result.Name === event.message.text
-      })
-
-      let reply = ''
-      for (const r of result) {
-        reply += `${r.Name} \n位置:${r.Add}\n特色:${r.Description} \n\n步行時間: ${r.Walkingtime}小時\n\n推薦附近景點: ${r.Landscape}\n\n注意事項: ${r.Remarks}`
+    const message = {
+      teyp: 'flex',
+      altText: '這是 flex',
+      contents: {
+        teyp: 'carousel',
+        contents: [flex]
       }
-
-      event.reply(reply)
-    } catch (error) {
-      event.reply('發生錯誤')
     }
+
+    fs.writeFileSync('cba.json', JSON.stringify(message, null, 2))
+    event.reply(message)
   }
 }
 )
